80bb59ce7f86ab304811497170c326c619b308c1
Adam_Diass <adamrpdiass@hotmail.com>
2025-08-03 15:23:03 -0300

    feat: adicionarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+ //<button class="button-add" onclick="adicionarAmigo()">Adicionar</button>
+
+$nomeAmigo = document.getElementById("amigo").value;
+
+$listaNomes = [];
+
+function adicionarAmigo()
+{
+    let nomeAmigo = document.getElementById("amigo").value;
+    $listaNomes.push(nomeAmigo);
+    console.log($listaNomes);
+}
+
